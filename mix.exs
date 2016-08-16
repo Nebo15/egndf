321d9824177d03e2f7a9c0fd0e2774ac5d87db75
paul-nebo <paul@nebo15.com>
2016-08-16 17:47:42 +0300

    added description

--- mix.exs
+++ mix.exs
@@ -5,6 +5,7 @@ defmodule Egndf.Mixfile do
     [app: :egndf,
      version: "0.1.0",
      elixir: "~> 1.3",
+     description: description(),
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
      deps: deps()]
