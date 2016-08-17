f1387b784b1f3cedc8951284bdae25b37ae3819c
paul.bardack <paul.bardak@gmail.com>
2016-08-17 11:07:24 +0300

    improved tests

--- test/egndf_test.exs
+++ test/egndf_test.exs
@@ -2,10 +2,55 @@ defmodule EgndfTest do
   use ExUnit.Case
   doctest Egndf
 
+  @table_id "57839fc6e79e85188b738860"
+
   test "make decision" do
-    a = Egndf.decision_make("57b2c934ce3c0c04f5688f3a", %{is_set: 123, second: "string"})
-    IO.inspect a
-    a = Egndf.decision_get("57b2d93bce3c0c04f5688f41")
-    IO.inspect a
+    decision = Egndf.decision_make(@table_id, %{number: 123, string: "string"})
+    assert_meta_200 decision
+    assert_decision decision
+
+    {:ok, decision} = decision
+    resp = Egndf.decision_get(decision["data"]["_id"])
+    assert_meta_200 resp
+    assert_decision resp
+  end
+
+  def assert_decision({:ok, data = %{
+    "data" => %{
+      "_id" => _,
+      "application" => _,
+      "title" => _,
+      "description" => _,
+      "final_decision" => _,
+      "request" => _,
+      "table" => %{
+        "_id" => _,
+        "description" => _,
+        "matching_type" => _,
+        "title" => _,
+        "variant" => %{
+          "_id" => _,
+          "description" => _,
+          "title" => _
+        }
+      },
+      "created_at" => _,
+      "updated_at" => _
+      }
+    }}
+    ) do
+    assert data
+  end
+
+  def assert_decision(_) do
+    assert false, "invalid decision"
+  end
+
+  def assert_meta_200({:ok, %{"meta" => %{"code" => code}}}) do
+    assert code == 200
+  end
+
+  def assert_meta_200(_) do
+    assert false, "invalid meta"
   end
 end
